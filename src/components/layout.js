a737cd04ebe94e9f1cb255972d25c4855fb55d28
ramprits <rampritsahani@outlook.in>
2019-02-18 07:21:44 +0530

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,81 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+const styles = theme => ({
+  appBar: {
+    position: "static"
+  },
+  toolbarTitle: {
+    flex: 1
+  },
+  linkStyle: {
+    linkStyle: "none"
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  }
+});
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+function Layout(props) {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            My Blog
+          </Typography>
+          <Button color="inherit">Support</Button>
+          <Button
+            color="inherit"
+            style={{ marginRight: "2px" }}
+            variant="outlined"
+          >
+            <Link
+              to="/login"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Login
+            </Link>
+          </Button>
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+          <Button color="inherit" variant="outlined">
+            <Link
+              to="/signup"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Signup
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.layout}>{props.children}</main>
+    </React.Fragment>
+  );
 }
 
-export default Layout
+Layout.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Layout);
---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import Login from "../components/login";
+class loginPage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Login />
+      </Fragment>
+    );
+  }
+}
+export default loginPage;
